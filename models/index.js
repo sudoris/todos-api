92494f51c74c24a6531adebd2785fde1730d3b57
dchen21 <zdent09@gmail.com>
2018-04-11 23:18:55 +0800

    linked to live mongodb database using mlab

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose.set("debug", true);
-mongoose.connect("mongodb://localhost/todo-api");
+// mongoose.connect("mongodb://localhost/todo-api");
+mongoose.connect("mongodb://dchen21:750322@ds241869.mlab.com:41869/todo-api");
+
 
 mongoose.Promise = Promise;
 
